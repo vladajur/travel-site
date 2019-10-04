2dfb77c2e996fd87aecc4dd0077b2e39cbf9a71a
Vlada <vladajur@gmail.com>
2019-10-04 17:01:44 +0200

    completed automatic sprite generation

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -6,12 +6,12 @@ nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
 mixins = require('postcss-mixins');
 
-gulp.task('styles' , function(){
-    return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport,mixins,cssvars,nested,autoprefixer]))
-    .on('error' , function(errorInfo){
-        console.log(errorInfo.toString());
-        this.emit('end');
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
     })
     .pipe(gulp.dest('./app/temp/styles'));
 });
